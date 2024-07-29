08afea512f18824ce060fa86c99ff53b56b00939
zhao <zhaorongchenginfo@yeah.net>
2024-07-29 23:33:50 +0900

    4.3 新增member会员模块

---
+++ member/src/main/java/com/zzpig/train/member/config/MemberApplication.java
@@ -0,0 +1,15 @@
+package com.zzpig.train.member.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.zzpig")
+public class MemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+
+}
