e8575ee81c8a48274ad7a3a01b316c5c2410df0d
zhao <zhaorongchenginfo@yeah.net>
2024-09-17 17:47:06 +0900

    4.8 增加common模块，增加了公共配置文件，需要放到resources/config/下

--- common/src/main/java/com/zzpig/train/common/aspect/LogAspect.java
+++ common/src/main/java/com/zzpig/train/common/aspect/LogAspect.java
@@ -23,7 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Component
 public class LogAspect {
     public LogAspect() {
-        System.out.println("Common LogAspect");
+        System.out.println("Zhao's Common LogAspect");
     }
 
     private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
