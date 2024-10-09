42591af60e2192232ba3eff01c399fcc43658ddc
zhao <zhaorongchenginfo@yeah.net>
2024-10-09 17:45:54 +0900

    4.20 集成雪花算法，实现自增不重复id

--- member/src/main/java/com/zzpig/train/member/service/MemberService.java
+++ member/src/main/java/com/zzpig/train/member/service/MemberService.java
@@ -1,8 +1,10 @@
 package com.zzpig.train.member.service;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.IdUtil;
 import com.zzpig.train.common.exception.BusinessException;
 import com.zzpig.train.common.exception.BusinessExceptionEnum;
+import com.zzpig.train.common.util.SnowUtil;
 import com.zzpig.train.member.domain.Member;
 import com.zzpig.train.member.domain.MemberExample;
 import com.zzpig.train.member.mapper.MemberMapper;
@@ -33,8 +35,9 @@ public class MemberService {
         }
 
         Member member = new Member();
-        member.setId(System.currentTimeMillis());
+//        member.setId(System.currentTimeMillis());
 //        member.setId(1728304023362L);
+        member.setId(SnowUtil.getSnowflakeNextId());
         member.setMobile(mobile);
 
         memberMapper.insert(member);
