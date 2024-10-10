0eefdd693b40e0e9c0612c83f7869b32ac700dc3
zhao <zhaorongchenginfo@yeah.net>
2024-10-10 17:02:35 +0900

    5.6 完成登陆前端页面开发，登陆包括注册功能

---
+++ web/src/views/login.vue
@@ -0,0 +1,71 @@
+<template>
+  <a-row class="login">
+    <a-col :span="8" offset="8" class="login-main">
+      <h1 class="centered-title">小卒科技-仿12306购票平台</h1>
+      <a-form
+          :model="loginForm"
+          name="basic"
+          autocomplete="off"
+          @finish="onFinish"
+          @finishFailed="onFinishFailed"
+      >
+        <a-form-item
+            label="手机号"
+            name="username"
+            :rules="[{ required: true, message: 'Please input your username!' }]"
+        >
+          <a-input v-model:value="loginForm.username" />
+        </a-form-item>
+
+        <a-form-item
+            label="验证码"
+            name="code"
+            :rules="[{ required: true, message: '请输入验证码' }]"
+        >
+          <a-input v-model:value="loginForm.code">
+            <template #addonAfter>
+              <a @click="sendCode">获取验证码</a>
+            </template>
+          </a-input>
+        </a-form-item>
+
+        <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+          <a-button type="primary" html-type="submit">登陆</a-button>
+        </a-form-item>
+      </a-form>
+    </a-col>
+  </a-row>
+</template>
+
+<script>
+import { defineComponent, reactive } from 'vue';
+export default defineComponent({
+  setup() {
+    const loginForm = reactive({
+      username: '',
+      code: '',
+    });
+    const onFinish = values => {
+      console.log('Success:', values);
+    };
+    const onFinishFailed = errorInfo => {
+      console.log('Failed:', errorInfo);
+    };
+    return {
+      loginForm,
+      onFinish,
+      onFinishFailed,
+    };
+  },
+});
+</script>
+
+<style>
+.centered-title {
+  font-weight: bold;
+  font-family: "黑体", sans-serif;
+  color: black;
+  text-align: center;
+  font-size:30px;
+}
+</style>
