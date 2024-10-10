6828ed4ded57055af1015736934e5fe347c75ae5
zhao <zhaorongchenginfo@yeah.net>
2024-10-10 18:09:22 +0900

    5.7 增加短信验证码接口

--- member/src/main/java/com/zzpig/train/member/controller/MemberController.java
+++ member/src/main/java/com/zzpig/train/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.zzpig.train.member.controller;
 
 import com.zzpig.train.common.resp.CommonResp;
 import com.zzpig.train.member.req.MemberRegisterReq;
+import com.zzpig.train.member.req.MemberSendCodeReq;
 import com.zzpig.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
@@ -31,6 +32,12 @@ public class MemberController {
 //        commonResp.setContent(register);
 //        return commonResp;
         return new CommonResp<>(register);
+    }
+
+    @PostMapping("/send-code")
+    public CommonResp<Long> sendCode(@Valid MemberSendCodeReq req){
+        memberService.sendCode(req);
+        return new CommonResp<>();
     }
 
 }
--- member/src/main/java/com/zzpig/train/member/service/MemberService.java
+++ member/src/main/java/com/zzpig/train/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.zzpig.train.member.service;
 
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.RandomUtil;
 import com.zzpig.train.common.exception.BusinessException;
 import com.zzpig.train.common.exception.BusinessExceptionEnum;
 import com.zzpig.train.common.util.SnowUtil;
@@ -9,7 +10,10 @@ import com.zzpig.train.member.domain.Member;
 import com.zzpig.train.member.domain.MemberExample;
 import com.zzpig.train.member.mapper.MemberMapper;
 import com.zzpig.train.member.req.MemberRegisterReq;
+import com.zzpig.train.member.req.MemberSendCodeReq;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +21,8 @@ import java.util.List;
 
 @Service
 public class MemberService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MemberService.class);
 
     @Autowired
     private MemberMapper memberMapper;
@@ -42,6 +48,30 @@ public class MemberService {
 
         memberMapper.insert(member);
         return member.getId();
+    }
+
+    public void sendCode(MemberSendCodeReq req){
+        String mobile = req.getMobile();
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        LOG.info("{}",list.size());
+        // 如果手机号不存在，则插入一条记录
+        if(CollUtil.isEmpty(list)){
+            LOG.info("手机号不存在，插入一条记录");
+            Member member = new Member();
+            member.setId(SnowUtil.getSnowflakeNextId());
+            member.setMobile(mobile);
+            memberMapper.insert(member);
+        }else{
+            LOG.info("手机号存在，不插入记录");
+        }
+        // 生成验证码
+        String code = RandomUtil.randomString(4);
+        LOG.info("生成验证码：{}",code);
+        // 保存短信记录表: 手机号，验证码，有效期，是否已使用，业务类型，发送时间，使用时间
+
+
     }
 
 }
