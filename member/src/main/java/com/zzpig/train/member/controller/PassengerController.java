6849f533877996831d980d1bdafc864c95ec7985
zhao <zhaorongchenginfo@yeah.net>
2024-10-18 16:18:40 +0900

    7.3 实现新增乘车人接口

---
+++ member/src/main/java/com/zzpig/train/member/controller/PassengerController.java
@@ -0,0 +1,25 @@
+package com.zzpig.train.member.controller;
+
+import com.zzpig.train.common.resp.CommonResp;
+import com.zzpig.train.member.req.MemberRegisterReq;
+import com.zzpig.train.member.req.PassengerSaveReq;
+import com.zzpig.train.member.service.PassengerService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/passenger")
+public class PassengerController {
+
+    @Autowired
+    PassengerService passengerService;
+
+    @PostMapping("/save")
+    public CommonResp<Long> save(@Valid PassengerSaveReq req){
+        passengerService.save(req);
+        return new CommonResp<>();
+    }
+}
---
+++ member/src/main/java/com/zzpig/train/member/service/PassengerService.java
@@ -0,0 +1,33 @@
+package com.zzpig.train.member.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.zzpig.train.common.exception.BusinessException;
+import com.zzpig.train.common.exception.BusinessExceptionEnum;
+import com.zzpig.train.common.util.SnowUtil;
+import com.zzpig.train.member.domain.Member;
+import com.zzpig.train.member.domain.Passenger;
+import com.zzpig.train.member.mapper.PassengerMapper;
+import com.zzpig.train.member.req.PassengerSaveReq;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PassengerService {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberService.class);
+
+    @Autowired
+    private PassengerMapper passengerMapper;
+
+    public void save(PassengerSaveReq req){
+        DateTime now = DateTime.now();
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        passenger.setId(SnowUtil.getSnowflakeNextId());
+        passenger.setCreateTime(now);
+        passenger.setUpdateTime(now);
+        passengerMapper.insert(passenger);
+    }
+}
