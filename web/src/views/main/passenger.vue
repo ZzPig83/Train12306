2302faba4e913e4472403bfcdfc2fc4a5b816059
zhao <zhaorongchenginfo@yeah.net>
2024-10-23 00:08:47 +0900

    7.16 乘车人编辑界面开发

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,7 +1,7 @@
 <template>
   <p>
     <a-space>
-      <a-button type="primary" @click="showModal">新增</a-button>
+      <a-button type="primary" @click="onAdd">新增</a-button>
       <a-button type="primary" @click="handleQuery()">刷新</a-button>
     </a-space>
   </p>
@@ -9,7 +9,15 @@
            :columns="columns"
            :pagination="pagination"
            @change="handleTableChange"
-           :loading="loading"/>
+           :loading="loading">
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.dataIndex ==='operation'">
+        <a-space>
+          <a @click="onEdit(record)">编辑</a>
+        </a-space>
+      </template>
+    </template>
+  </a-table>
   <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk" ok-text="确认" cancel-text="取消">
     <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{span: 32}">
       <a-form-item label="姓名">
@@ -31,14 +39,14 @@
 
 <script>
 
-import {defineComponent, onMounted, reactive, ref} from "vue";
+import {defineComponent, onMounted, ref} from "vue";
 import axios from "axios";
 import {notification} from "ant-design-vue";
 
 export default defineComponent({
   setup(){
     const visible = ref(false);
-    const passenger =reactive({
+    const passenger =ref({
       id: undefined,
       memberId: undefined,
       name: undefined,
@@ -49,7 +57,7 @@ export default defineComponent({
     });
     const passengers = ref([]);
     // 分页的三个属性名是固定的
-    const pagination = reactive({
+    const pagination = ref({
       total: 0,
       current: 1,
       pageSize: 5,
@@ -70,22 +78,32 @@ export default defineComponent({
         dataIndex: 'type',
         key: 'type',
       },
+      {
+        title: '操作',
+        dataIndex: 'operation'
+      }
     ];
     let loading = ref(false);
 
-    const showModal = () =>{
+    const onAdd = () =>{
+      passenger.value = {};
+      visible.value = true;
+    };
+
+    const onEdit = (record) => {
+      passenger.value = window.Tool.copy(record);
       visible.value = true;
     };
 
     const handleOk = () =>{
-      axios.post("/member/passenger/save",passenger).then(response =>{
+      axios.post("/member/passenger/save",passenger.value).then(response =>{
         let data = response.data;
         if(data.success){
           notification.success({description: "保存成功"});
           visible.value = false;
           handleQuery({
             page: 1,
-            size: pagination.pageSize
+            size: pagination.value.pageSize
           })
         }else{
           notification.error({description: data.message});
@@ -97,7 +115,7 @@ export default defineComponent({
       if (!param){
         param = {
           page: 1,
-          size: pagination.pageSize
+          size: pagination.value.pageSize
         };
       }
       loading.value = true;
@@ -112,8 +130,8 @@ export default defineComponent({
         if(data.success){
           passengers.value = data.content.list;
           // 设置分页控件的值
-          pagination.current = param.page;
-          pagination.total = data.content.total;
+          pagination.value.current = param.page;
+          pagination.value.total = data.content.total;
         } else {
           notification.error({description: data.message});
         }
@@ -130,7 +148,7 @@ export default defineComponent({
     onMounted(() =>{
       handleQuery(({
         page: 1,
-        size: pagination.pageSize
+        size: pagination.value.pageSize
       }))
     });
 
@@ -138,7 +156,8 @@ export default defineComponent({
       visible,
       passenger,
       pagination,
-      showModal,
+      onAdd,
+      onEdit,
       handleOk,
       handleQuery,
       handleTableChange,
