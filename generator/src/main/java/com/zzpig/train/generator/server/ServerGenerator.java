a32a5900335f01064a1a04ec4ae442a8c970496c
zhao <zhaorongchenginfo@yeah.net>
2024-10-24 21:28:59 +0900

    8.5 使用PassengerService制作模板，生成service成功

--- generator/src/main/java/com/zzpig/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/zzpig/train/generator/server/ServerGenerator.java
@@ -1,5 +1,6 @@
 package com.zzpig.train.generator.server;
 
+import com.zzpig.train.generator.util.FreemarkerUtil;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
@@ -15,15 +16,22 @@ public class ServerGenerator {
 //    static boolean readOnly = true;
 //    static String vuePath = "admin/src/views/main/";
 //    static String serverPath = "[module]/src/main/java/com/jiawa/train/[module]/";
-    static String toPath = "generator\\src\\main\\java\\com\\zzipg\\train\\generator\\ftl";
+    static String servicePath = "[module]/src/main/java/com/zzpig/train/[module]/service/";
     static String pomPath = "generator/pom.xml";
 //    static String module = "";
     static {
-        new File(toPath).mkdirs();
+        new File(servicePath).mkdirs();
     }
 
     public static void main(String[] args) throws Exception {
+        // 获取mybatis-generator
         String generatorPath = getGeneratorPath();
+        // 比如generator-config-member.xml，得到module = member
+        String module = generatorPath.replace("src/main/resources/generator-config-", "").replace(".xml", "");
+        System.out.println("module: " + module);
+        servicePath = servicePath.replace("[module]", module);
+        // new File(servicePath).mkdirs();
+        System.out.println("servicePath: " + servicePath);
 
         Document document = new SAXReader().read("generator/" + generatorPath);
         Node table = document.selectSingleNode("//table");
@@ -31,6 +39,33 @@ public class ServerGenerator {
         Node tableName = table.selectSingleNode("@tableName");
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+
+        // 示例：表名 jiawa_test
+        // Domain = JiawaTest
+        String Domain = domainObjectName.getText();
+        // domain = jiawaTest
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        // do_main = jiawa-test
+        String do_main = tableName.getText().replaceAll("_", "-");
+//        // 表中文名
+//        String tableNameCn = DbUtil.getTableComment(tableName.getText());
+//        List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
+//        Set<String> typeSet = getJavaTypes(fieldList);
+
+        // 组装参数
+        Map<String, Object> param = new HashMap<>();
+        param.put("module", module);
+        param.put("Domain", Domain);
+        param.put("domain", domain);
+        param.put("do_main", do_main);
+//        param.put("tableNameCn", tableNameCn);
+//        param.put("fieldList", fieldList);
+//        param.put("typeSet", typeSet);
+//        param.put("readOnly", readOnly);
+        System.out.println("组装参数：" + param);
+
+        FreemarkerUtil.initConfig("service.ftl");
+        FreemarkerUtil.generator(servicePath + Domain + "Service.java", param);
     }
 
     private static String getGeneratorPath() throws DocumentException {
--- member/src/main/java/com/zzpig/train/member/service/PassengerService.java
+++ member/src/main/java/com/zzpig/train/member/service/PassengerService.java
@@ -3,70 +3,64 @@ package com.zzpig.train.member.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
-import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.zzpig.train.common.context.LoginMemberContext;
-import com.zzpig.train.common.exception.BusinessException;
-import com.zzpig.train.common.exception.BusinessExceptionEnum;
 import com.zzpig.train.common.resp.PageResp;
 import com.zzpig.train.common.util.SnowUtil;
-import com.zzpig.train.member.domain.Member;
 import com.zzpig.train.member.domain.Passenger;
 import com.zzpig.train.member.domain.PassengerExample;
 import com.zzpig.train.member.mapper.PassengerMapper;
 import com.zzpig.train.member.req.PassengerQueryReq;
 import com.zzpig.train.member.req.PassengerSaveReq;
 import com.zzpig.train.member.resp.PassengerQueryResp;
+import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PassengerService {
-    private static final Logger LOG = LoggerFactory.getLogger(MemberService.class);
 
-    @Autowired
+    private static final Logger LOG = LoggerFactory.getLogger(PassengerService.class);
+
+    @Resource
     private PassengerMapper passengerMapper;
 
-    public void save(PassengerSaveReq req){
+    public void save(PassengerSaveReq req) {
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
-        if (ObjectUtil.isNull(passenger.getId())) { // 如果是id是空，则新增
-            passenger.setMemberId(LoginMemberContext.getId());
-            LOG.info("会员memberId是{}",LoginMemberContext.getId());
+        if (ObjectUtil.isNull(passenger.getId())) {
             passenger.setId(SnowUtil.getSnowflakeNextId());
             passenger.setCreateTime(now);
             passenger.setUpdateTime(now);
             passengerMapper.insert(passenger);
-        } else { // 如果id不空，则更新
+        } else {
             passenger.setUpdateTime(now);
             passengerMapper.updateByPrimaryKey(passenger);
         }
     }
 
-    public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req){
+    public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req) {
         PassengerExample passengerExample = new PassengerExample();
         passengerExample.setOrderByClause("id desc");
         PassengerExample.Criteria criteria = passengerExample.createCriteria();
-        if(ObjectUtil.isNotNull(req.getMemberId())){
-            criteria.andMemberIdEqualTo(req.getMemberId());
-        }
+
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
         PageHelper.startPage(req.getPage(), req.getSize());
         List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
 
-        PageInfo<Passenger> pageInfo= new PageInfo<>(passengerList);
-        LOG.info("总行数：{}",pageInfo.getTotal());
-        LOG.info("总页数：{}",pageInfo.getPages());
+        PageInfo<Passenger> pageInfo = new PageInfo<>(passengerList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 
         List<PassengerQueryResp> list = BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
+
         PageResp<PassengerQueryResp> pageResp = new PageResp<>();
         pageResp.setTotal(pageInfo.getTotal());
         pageResp.setList(list);
-
         return pageResp;
     }
 
