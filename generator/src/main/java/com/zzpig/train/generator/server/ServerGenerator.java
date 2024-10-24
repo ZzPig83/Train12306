688690bb689086d88131828c748f613827ff7a38
zhao <zhaorongchenginfo@yeah.net>
2024-10-24 17:44:50 +0900

    8.4 集成dom4j，读取pom.xml，得到当前generator.xml

---
+++ generator/src/main/java/com/zzpig/train/generator/server/ServerGenerator.java
@@ -0,0 +1,33 @@
+package com.zzpig.train.generator.server;
+
+import org.dom4j.Document;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ServerGenerator {
+//    static boolean readOnly = true;
+//    static String vuePath = "admin/src/views/main/";
+//    static String serverPath = "[module]/src/main/java/com/jiawa/train/[module]/";
+    static String toPath = "generator\\src\\main\\java\\com\\zzipg\\train\\generator\\ftl";
+    static String pomPath = "generator/pom.xml";
+//    static String module = "";
+    static {
+        new File(toPath).mkdirs();
+    }
+
+    public static void main(String[] args) throws Exception {
+        SAXReader saxReader = new SAXReader();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put ("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(pomPath);
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
+    }
+}
--- web/vue.config.js
+++ web/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  devServer:{
+    client: {
+      webSocketURL: 'ws://192.168.150.26:9000/ws'
+    }
+  }
 })
