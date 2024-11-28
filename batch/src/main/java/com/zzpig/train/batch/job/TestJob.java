1abcb033f8a1ddeac78ad3f9896c979768767f62
zhao <zhaorongchenginfo@yeah.net>
2024-11-28 16:30:56 +0900

    10.5 集成quartz，和springboot自带的定时任务可以共存

---
+++ batch/src/main/java/com/zzpig/train/batch/job/TestJob.java
@@ -0,0 +1,14 @@
+package com.zzpig.train.batch.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class TestJob implements Job {
+
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("TestJob TEST");
+    }
+}
