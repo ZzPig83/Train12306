1c3a3a63572c8ccba561b9b602480e51898a3e7c
zhao <zhaorongchenginfo@yeah.net>
2024-12-09 09:56:48 +0900

    12.12 后端增加确认下单购票接口

--- business/src/main/java/com/zzpig/train/business/controller/DailyTrainTicketController.java
+++ business/src/main/java/com/zzpig/train/business/controller/DailyTrainTicketController.java
@@ -1,41 +1,27 @@
 package com.zzpig.train.business.controller;
 
-import com.zzpig.train.common.context.LoginMemberContext;
-import com.zzpig.train.common.resp.CommonResp;
-import com.zzpig.train.common.resp.PageResp;
-//import com.zzpig.train.business.req.MemberRegisterReq;
 import com.zzpig.train.business.req.DailyTrainTicketQueryReq;
-import com.zzpig.train.business.req.DailyTrainTicketSaveReq;
 import com.zzpig.train.business.resp.DailyTrainTicketQueryResp;
 import com.zzpig.train.business.service.DailyTrainTicketService;
+import com.zzpig.train.common.resp.CommonResp;
+import com.zzpig.train.common.resp.PageResp;
+import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin/daily-train-ticket")
+@RequestMapping("/daily-train-ticket")
 public class DailyTrainTicketController {
 
-    @Autowired
-    DailyTrainTicketService dailyTrainTicketService;
-
-    @PostMapping("/save")
-    public CommonResp<Long> save(@Valid @RequestBody DailyTrainTicketSaveReq req){
-    dailyTrainTicketService.save(req);
-        return new CommonResp<>();
-    }
+    @Resource
+    private DailyTrainTicketService dailyTrainTicketService;
 
     @GetMapping("/query-list")
-    public CommonResp<PageResp<DailyTrainTicketQueryResp>> queryList(@Valid DailyTrainTicketQueryReq req){
+    public CommonResp<PageResp<DailyTrainTicketQueryResp>> queryList(@Valid DailyTrainTicketQueryReq req) {
         PageResp<DailyTrainTicketQueryResp> list = dailyTrainTicketService.queryList(req);
         return new CommonResp<>(list);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public CommonResp<Object> delete(@PathVariable Long id){
-        dailyTrainTicketService.delete(id);
-        return new CommonResp<>();
-    }
 }
--- business/src/main/java/com/zzpig/train/business/controller/TrainController.java
+++ business/src/main/java/com/zzpig/train/business/controller/TrainController.java
@@ -1,59 +1,26 @@
 package com.zzpig.train.business.controller;
 
-import com.zzpig.train.business.service.TrainSeatService;
-import com.zzpig.train.common.context.LoginMemberContext;
-import com.zzpig.train.common.resp.CommonResp;
-import com.zzpig.train.common.resp.PageResp;
-//import com.zzpig.train.business.req.MemberRegisterReq;
-import com.zzpig.train.business.req.TrainQueryReq;
-import com.zzpig.train.business.req.TrainSaveReq;
 import com.zzpig.train.business.resp.TrainQueryResp;
 import com.zzpig.train.business.service.TrainService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import com.zzpig.train.common.resp.CommonResp;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin/train")
+@RequestMapping("/train")
 public class TrainController {
 
-    @Autowired
-    TrainService trainService;
-
-    @Autowired
-    TrainSeatService trainSeatService;
-
-    @PostMapping("/save")
-    public CommonResp<Long> save(@Valid @RequestBody TrainSaveReq req){
-        trainService.save(req);
-        return new CommonResp<>();
-    }
-
-    @GetMapping("/query-list")
-    public CommonResp<PageResp<TrainQueryResp>> queryList(@Valid TrainQueryReq req){
-        PageResp<TrainQueryResp> list = trainService.queryList(req);
-        return new CommonResp<>(list);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public CommonResp<Object> delete(@PathVariable Long id){
-        trainService.delete(id);
-        return new CommonResp<>();
-    }
+    @Resource
+    private TrainService trainService;
 
     @GetMapping("/query-all")
-    public CommonResp<List<TrainQueryResp>> queryAll(){
+    public CommonResp<List<TrainQueryResp>> queryList() {
         List<TrainQueryResp> list = trainService.queryAll();
         return new CommonResp<>(list);
     }
-
-    @GetMapping("/gen-seat/{trainCode}")
-    public CommonResp<Object> genSeat(@PathVariable String trainCode){
-        trainSeatService.genTrainSeat(trainCode);
-        return new CommonResp<>();
-    }
-
 
 }
